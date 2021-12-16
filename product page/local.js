0f9b6c9fbab078e54ef80da73249fff5956f6f75
MadhanM282 <96064381+MadhanM282@users.noreply.github.com>
2021-12-16 15:37:31 +0530

    .

--- product page/local.js
+++ product page/local.js
@@ -8,4 +8,28 @@ document.querySelector("#cart").addEventListener("click",cart)
 function cart(){
     window.location.href = "cart.html"
 }
+document.querySelector("#buy").addEventListener("click",data)
 
+var arr = JSON.parse(localStorage.getItem("cartvalues"));
+data(arr);
+
+function data(){
+    var name = document.querySelector("h1").textContent;
+    var imag = document.querySelector("#image");
+    var image = imag.querySelector("img");
+    var ide = document.querySelector("#ID");
+     var id = ide.textContent;
+    var price = document.querySelector(".pric").textContent;
+    var strpric = document.querySelector(".str").textContent;
+
+    var obj = {
+        nam : name,
+        image:image,
+        id:id,
+        price:price,
+        strpric:strpric,
+    }
+    arr.push(obj);
+
+    localStorage.setItem("cartvalues",JSON.stringify(arr));
+}
